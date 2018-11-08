d4ce16973014df1492a531ce94839079c8aa9f6f
squaresmile <squaresmile@protonmail.com>
2018-11-08 14:04:33 -0500

    Add embers count in table

--- app.py
+++ app.py
@@ -2,15 +2,41 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import numpy as np
 
 EXP_df = pd.read_csv("level.csv")
+TABLE = [
+    ["Full EXP", 0, 0, 0],
+    ["1/2 EXP", 0, 0, 0],
+    ["1/3 EXP", 0, 0, 0]
+]
+OUTPUT_df = pd.DataFrame(
+    TABLE,
+    columns = ["How much", "EXP", "Class Embers", "Non-class Embers"]
+)
+CLASS_EXP = 32400
+NON_CLASS_EXP = 27000
 
 def exp_calc(from_level, to_level, residual=0):
-	if residual == 0:
-		exp = EXP_df.iloc[from_level:to_level, 1].sum()
-	else:
-		exp = EXP_df.iloc[from_level+1:to_level, 1].sum() + residual
-	return exp
+    if from_level == to_level:
+        return 0
+    else:
+        if residual == 0:
+            exp = EXP_df.iloc[from_level:to_level, 1].sum()
+        else:
+            exp = EXP_df.iloc[from_level+1:to_level, 1].sum() + residual
+        return exp
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
 app = dash.Dash(__name__)
 server = app.server
@@ -19,17 +45,9 @@ app.layout = html.Div(children=[
     
     html.H1(children='FGO EXP Calculator'),
     
-    html.Div(children='From level:'),
-
-    dcc.Input(id='from-level', value=1, type='number', min=1, max=100),
-
-    html.Div(children='To level:'),
-
-    dcc.Input(id='to-level', value=50, type='number', min=1, max=100),
-
-    html.Div(children='Residual EXP:'),
-
-    dcc.Input(id='residual-exp', value=0, type='number', min=1, max=100),
+    html.Div(children='From level:'), dcc.Input(id='from-level', value=1, type='number', min=1, max=100),
+    html.Div(children='To level:'), dcc.Input(id='to-level', value=50, type='number', min=1, max=100),
+    html.Div(children='Residual EXP:'), dcc.Input(id='residual-exp', value=0, type='number', min=1, max=100),
 
     html.Div(id='exp-needed')
 ], className="container"
@@ -45,11 +63,12 @@ def update_exp_text(from_level, to_level, residual_exp):
         return "From level can\'t be higher than to level."
     else:
         exp_needed = exp_calc(from_level, to_level, residual_exp)
-        return html.Div([
-            html.P("EXP Needed: {:,}".format(exp_needed)),
-            html.P("1/2 EXP: {:,.0f}".format(exp_needed/2)),
-            html.P("1/3 EXP: {:,.0f}".format(exp_needed/3))
-        ])
+        exp = np.array([exp_needed, exp_needed/2, exp_needed/3])
+        OUTPUT_df['EXP'] = exp
+        OUTPUT_df["Class Embers"] = np.ceil(exp / CLASS_EXP)
+        OUTPUT_df["Non-class Embers"] = np.ceil(exp / NON_CLASS_EXP)
+        OUTPUT_df['EXP'] = OUTPUT_df['EXP'].apply(lambda x: "{:,.0f}".format(x))
+        return generate_table(OUTPUT_df)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
